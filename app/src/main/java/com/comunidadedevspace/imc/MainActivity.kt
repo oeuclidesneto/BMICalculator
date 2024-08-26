742175fdb5a5e4c03511fe11ba7c59d41f2e99d3
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2024-08-26 19:31:17 +0930

    Initial Screen editText

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,23 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edt_weight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.edt_height)
+        val btn_calculate = findViewById<Button>(R.id.btn_calculate)
+
+        btn_calculate.setOnClickListener {
+            val weight = edtWeight.text
+            val height = edtHeight.text
+            println ("Euclides clicks " + height)
+        }
+
     }
 }
